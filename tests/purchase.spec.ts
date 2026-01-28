22f483626f6510d1cb2ba648e0ce39607bf655ac
Michelle Harrell <abyssal81@gmail.com>
2026-01-28 12:29:53 -0700

    Utils change

--- tests/purchase.spec.ts
+++ tests/purchase.spec.ts
@@ -3,32 +3,42 @@ import { LoginPage } from '../pages/LoginPage.pages';
 import { InventoryPage } from '../pages/InventoryPage.pages';
 import { CheckoutPage } from '../pages/CheckoutPage.pages';
 import { UserData } from '../utils/UserData';
+import { verifyShippingInfo } from '../utils/UserData';
+
 
 test('End-to-End Purchase Flow', async ({ page }) => {
   const loginPage = new LoginPage(page);
   const inventoryPage = new InventoryPage(page);
   const checkoutPage = new CheckoutPage(page);
 
-  // Login
+  //Login
   await loginPage.goto();
   await loginPage.login(UserData.username, UserData.password);
   await expect(page).toHaveURL(/.*inventory.html/);
 
-  // Add item to cart
+  //Add item to cart
   await inventoryPage.addItemToCart('Sauce Labs Backpack');
   await inventoryPage.expectCartBadge(1);
   await inventoryPage.openCart();
   await inventoryPage.expectItemInCart('Sauce Labs Backpack');
 
-  // Checkout
+  //Checkout
   await page.getByRole('button', { name: 'Checkout' }).click();
   await checkoutPage.fillCheckoutInfo(UserData.firstName, UserData.lastName, UserData.zipCode);
 
-  // Verify correct item is present on checkout step two
+  //Verify correct item is present on checkout page
   const cartItem = await inventoryPage.cartItem('Sauce Labs Backpack');
   await expect(cartItem).toContainText('Sauce Labs Backpack');
 
-  // Verify order totals (regex for example, will not return actual totals in this state)
+  //Verify payment and shipping information
+  await verifyShippingInfo(page, [
+    'Payment Information',
+    'SauceCard #31337',
+    'Shipping Information',
+    'FREE PONY EXPRESS DELIVERY!',
+  ]);
+
+  //Verify order totals (regex for example, will not return actual totals in this state)
   await expect(page.getByText(/Item total: \$\d+\.\d{2}/)).toBeVisible();
   await expect(page.getByText(/Tax: \$\d+\.\d{2}/)).toBeVisible();
   await expect(page.getByText(/Total: \$\d+\.\d{2}/)).toBeVisible();
--- utils/UserData.ts
+++ utils/UserData.ts
@@ -6,5 +6,14 @@ export const UserData = {
     zipCode: '85283'
   };
   
- 
+  import { expect, Page } from '@playwright/test';
+
+  export async function verifyShippingInfo(
+    page: Page,
+    texts: string[],
+  ) {
+    for (const text of texts) {
+      await expect(page.getByText(text)).toBeVisible();
+    }
+  }
   
